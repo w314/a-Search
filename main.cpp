676557212bfe52421aee69ce551941bdf325a137
vagrant <vagrant@vagrant.vm>
2021-07-12 20:49:52 +0000

    feat: Reads board from file

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <string>
 using std::cout;
 using std::vector;
+using std::ifstream;
+using std::string;
 
 
 void PrintBoard(vector<vector<int>> board) {
@@ -16,6 +20,15 @@ void PrintBoard(vector<vector<int>> board) {
 
 int main() {
 
+	string path = "./1.board";
+	ifstream my_file(path); 
+
+	if(my_file) {
+		string line;
+		while(getline(my_file, line)) {
+			cout << line << "\n";
+		}
+	}
 	vector<vector<int>> board {
 		{0, 1, 0, 0, 0, 0},
 		{0, 1, 0, 0, 0, 0},
@@ -23,29 +36,6 @@ int main() {
 		{0, 1, 0, 0, 0, 0},
 		{0, 0, 0, 0, 1, 0}
 	};
-
-	// cout << board.size() << " x " << board[0].size() << "\n";
-
-	// for(int i = 0; i < board[0].size(); i++) {
-	// 	cout << board[0][i];
-	// }
-
-	// cout << "\n";
-
-	// for(int i: board[0]) {
-	// 	cout << i;
-	// }
-
-	// cout << "\n" << "\n";
-
-	// for(vector<int> row : board) {
-	// 	for(int i : row) {
-	// 		cout << i;
-	// 	}
-	// 	cout << "\n";
-	// }
-
-	// cout << "\n";
 
 	PrintBoard(board);
 }
